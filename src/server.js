16def439077cc526c555aa5c0da6005f0f1675ab
J <jbelding16@gmail.com>
2021-10-26 14:08:44 -0400

    Added skeleton of questions and answers get request handlers

--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const { connectDb } = require('./db/index');
-// const { Product, Question, Answer, Answers } = require('./db/index');
+const {
+  Product,
+  // Question, Answer,
+  Answers,
+} = require('./db/index');
 require('dotenv').config();
 
 const app = express();
@@ -15,14 +19,42 @@ connectDb()
     });
   });
 
-// app.get('/qa/questions', (req, res) => {
-//   const { params } = req;
-//   let response = null;
-//   Product.findOne({ product_id: params.product_id }, (err, product) => {
-//     if (err) throw err;
-//     response = product;
-//     response.results.map((question) => {
-//       Answers.findOne({ })
-//     })
+app.get('/qa/questions', (req, res) => {
+  const { params } = req;
+  let response = null;
+  Product.findOne({ product_id: params.product_id }, (err, product) => {
+    if (err) throw err;
+    response = product;
+    response.results.forEach((question) => {
+      const q = question;
+      Answers.findOne({ question: question.question_id }, (answers) => {
+        if (err) throw err;
+        q.answers = answers.results;
+      });
+    });
+    res.status(200).send(response);
+  });
+});
+// eslint-disable-next-line max-len
+// const answers = response.results.map((question) => Answers.findOne({ question: question.question_id }));
+// Promise.all(answers)
+//   .then((eachAnswer) => {
+//     eachAnswer.forEach((answer) => {
+//       const a = {
+//         id: answer.answer_id,
+//         body: answer.body,
+//         date: answer.date,
+//         answerer_name: answer.answerer_name,
+//         helpfulness: answer.helpfulness,
+//         photos: answer.photos,
+//       };
+//       question.answers[a.id] = a;
+//     });
 //   });
-// });
+
+app.get('/qa/questions/:question_id?/answers', (req, res) => {
+  const { params } = req;
+  Answers.findOne({ question: params.question_id }, (err, answers) => {
+    res.status(200).send(JSON.stringify(answers));
+  });
+});
