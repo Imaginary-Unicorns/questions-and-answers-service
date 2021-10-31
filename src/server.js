9d1e90a9b343188b77785363bf9e910f6915bdd1
J <jbelding16@gmail.com>
2021-10-30 22:10:27 -0400

    Added get handlers for questions and answers, connected to client

--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const { connectDb } = require('./db/index');
 const {
-  // Product,
+  // Product, Answers,
   Question, Answer, Photos,
-  // Answers,
 } = require('./db/index');
+
 require('dotenv').config();
 
 const app = express();
@@ -19,6 +19,7 @@ connectDb()
     });
   });
 
+// -----GET QUESTIONS-----
 app.get('/qa/questions', async (req, res) => {
   const { query } = req;
   let count = 5;
@@ -33,15 +34,16 @@ app.get('/qa/questions', async (req, res) => {
     product_id: query.product_id,
   };
 
-  async function dbQuery() {
+  async function questionQuery() {
     const questions = await Question.find({ product_id: query.product_id })
       .limit(count)
       .skip(page * count)
       .exec();
 
     const withAnswers = questions.map(async (q) => {
+      const questionId = q.question_id;
       const question = {
-        question_id: q.question_id,
+        question_id: questionId,
         question_body: q.question_body,
         question_date: q.question_date,
         asker_name: q.asker_name,
@@ -52,14 +54,20 @@ app.get('/qa/questions', async (req, res) => {
       const findAnswers = await Answer.find({ question_id: q.question_id.toString() });
       findAnswers.forEach(async (answer) => {
         const id = answer.answer_id;
-        const photos = await Photos.findOne({ answer_id: id.toString() });
+        let photos = {};
+        const findPhotos = await Photos.findOne({ answer_id: id.toString() });
+        if (findPhotos) {
+          photos = findPhotos;
+        } else {
+          photos.urls = [];
+        }
         const transformedAnswer = {
           id,
           body: answer.body,
           date: answer.date,
           answerer_name: answer.answerer_name,
           helpfulness: answer.helpfullness,
-          photos,
+          photos: photos.urls,
         };
         answers[id] = transformedAnswer;
       });
@@ -71,7 +79,7 @@ app.get('/qa/questions', async (req, res) => {
 
   async function send() {
     try {
-      await dbQuery()
+      await questionQuery()
         .then((questions) => {
           Promise.all(questions)
             .then((results) => {
@@ -86,9 +94,65 @@ app.get('/qa/questions', async (req, res) => {
   send();
 });
 
-// app.get('/qa/questions/:question_id?/answers', (req, res) => {
-//   const { params } = req;
-//   Answers.findOne({ question: params.question_id }, (err, answers) => {
-//     res.status(200).send(JSON.stringify(answers));
-//   });
-// });
+// -----GET ANSWERS-----
+app.get('/qa/questions/:question_id?/answers', (req, res) => {
+  const questionId = req.params.question_id.toString();
+  const { query } = req;
+  let count = 5;
+  let page = 0;
+  if (query.count) {
+    count = query.count;
+  }
+  if (query.page) {
+    page = query.page - 1;
+  }
+  const response = {
+    question: questionId,
+    page,
+    count,
+  };
+
+  async function answerQuery() {
+    const findAnswers = await Question.find({ question_id: questionId })
+      .limit(count)
+      .skip(page * count)
+      .exec();
+
+    const results = findAnswers.map(async (answer) => {
+      const id = answer.answer_id;
+      let photos = {};
+      const findPhotos = await Photos.findOne({ answer_id: id.toString() });
+      if (findPhotos) {
+        photos = findPhotos;
+      } else {
+        photos.urls = [];
+      }
+      const transformedAnswer = {
+        answer_id: id,
+        body: answer.body,
+        date: answer.date,
+        answerer_name: answer.answerer_name,
+        helpfulness: answer.helpfullness,
+        photos: photos.urls,
+      };
+      return transformedAnswer;
+    });
+    return results;
+  }
+
+  async function send() {
+    try {
+      await answerQuery()
+        .then((answers) => {
+          Promise.all(answers)
+            .then((results) => {
+              response.results = results;
+              res.status(200).send(response);
+            });
+        });
+    } catch (err) {
+      res.status(500).send({ message: err });
+    }
+  }
+  send();
+});
